b5723924cc0a872d4202ef436af127b3ce0cffd8
PArmand5 <armands@priedes.eu>
2024-11-23 13:23:31 +0200

    commit Add new Python examples and update documentation

---
+++ 1.LanguageEssentials/Essentials.py
@@ -0,0 +1,15 @@
+#From lecture
+#iegūstam koda bāzi no github/gitlab
+git clone <repository url>
+ 
+#apskatam, kādas izmaiņas kuros failos esam veikuši
+git status -m "izmaiņu apraksts"
+ 
+#atlasam failus, kurus gribam sūtīt uz github
+git add
+ 
+#akceptējam izmaiņas lokāli
+git commit
+ 
+#sūtam izmaiņas uz github
+git push origin main
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # VApysec24
+iegūstam koda bāzi no github/gitlab
+git clone <repository url>
+ 
+apskatam, kādas izmaiņas kuros failos esam veikuši
+git status
+ 
+atlasam failus, kurus gribam sūtīt uz github
+git add
+ 
+akceptējam izmaiņas lokāli
+git commit
+ 
+sūtam izmaiņas uz github
+git push origin main
+
